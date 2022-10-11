f9270b686abb02f59e55e9487d0e30a2193e1a01
Casper Werner <cappe1996@hotmail.com>
2022-10-11 17:51:25 +0200

    Fastnat nu i checkIfCorrectkoden

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ let fromI = 0;
 
 
 /**********JUST INSERTS THE INSTRUCTION PARAGRAF**********/
-function writingtext(){
+function writingtext(location, text){
   if(fromI<insText.length){
     paragrafDiv.innerHTML += insText.charAt(fromI);
     fromI++;
@@ -22,21 +22,60 @@ writingtext();
 /*****************LIST OF WORDS THAT WE'RE GOING TO USE FOR THE GAME*************/
 
 const listOfWords = [
-  "Sallad",
-  "Kiwi",
-  "Juice",
-  "Hundra",
-  "Vatten",
-  "Spaghetti",
-  "Promenera"
+  "sallad",
+  "kiwi",
+  "juice",
+  "hundra",
+  "vatten",
+  "spaghetti",
+  "promenera"
   ];
 
   /**********************SLUMPMÄSSIGT TAR VI UT ETT ORD FRÅN LISTAN****************/
 var choosenWord = listOfWords[Math.floor(Math.random()*listOfWords.length)];
 
+const correctLetters = [];
+const incorrectLetters =[];
 
-
+/**************** GENOM ETT KLICK TAR VI BORT STARTPAGEN OCH VIDARE TILL GAMEPAGEN***************/
 startBtn.addEventListener("click",function(){
   startPage.style.display ="none";
   gamePage.style.display ="block";
-})
+});
+
+
+
+
+const submitBtn = document.querySelector("#Submit");
+const inputLetter = document.querySelector("#inputLetter");
+const wordDisplayer = document.querySelector("#wordDisplayer")
+
+
+var lettersOfWord = choosenWord.split("");
+// break down the word in letters
+//loop to see if the letter is in the array of letters
+function splitWord(){
+  wordDisplayer.innerHTML += lettersOfWord;
+  wordDisplayer.style.display="none";
+}
+splitWord();
+
+
+function checkIfCorrect(){
+  for(const letter of lettersOfWord)
+  {
+  
+    if(inputLetter.value ===letter){
+      console.log("true");
+      letter.style.display="block";
+      return
+    }
+    
+  }
+  console.log("false");
+
+}
+submitBtn.addEventListener("click", function(){
+  checkIfCorrect();
+});
+
